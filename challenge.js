861e77601e65ba871fd0b11b0f15a3e449d2b036
MEC4RNG <pgpolarized@gmail.com>
2025-06-15 13:54:11 -0400

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -178,7 +178,7 @@ function getRandom(arr) {
   }
   
   function generateChallenge(container) {
-    challengeOutput.innerHTML = "";
+    container.innerHTML = "";
   
     // Key
     const key = document.getElementById("keySelect").value;
